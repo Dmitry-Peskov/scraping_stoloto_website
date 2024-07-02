3e897e3739c9926855efef39c66442045f72c55c
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-07-02 21:58:07 +0300

    Add module models.py; Create BaseTicketModel

---
+++ database/models.py
@@ -0,0 +1,48 @@
+import datetime
+
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+from sqlalchemy.types import DateTime, Integer, SmallInteger
+from sqlalchemy import UniqueConstraint
+
+
+class BaseTicketModel(AsyncAttrs, DeclarativeBase):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__
+
+    @declared_attr
+    def __table_args__(cls):
+        return (UniqueConstraint('lottery_number', name=f'_{cls.__name__}_lottery_number'),)
+
+    lottery_number: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        unique=True,
+        index=True,
+        autoincrement=False
+    )
+    event_time: Mapped[datetime.datetime] = mapped_column(
+        DateTime,
+        nullable=False
+    )
+    year: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False
+    )
+    month: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False
+    )
+    day: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False
+    )
+    hour: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False
+    )
+    minute: Mapped[int] = mapped_column(
+        SmallInteger,
+        nullable=False
+    )
