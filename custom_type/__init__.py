d351195570df6a95eb527146e4ee07c63bb60d5f
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-07-07 21:36:17 +0300

    Add namedtuple: "Numbers7x49", "Numbers6x45", "Numbers5x36"

--- custom_type/__init__.py
+++ custom_type/__init__.py
@@ -1,7 +1,10 @@
 __all__ = [
     "LotteryNames",
     "LotteryNumber",
-    "DateTimeElements"
+    "DateTimeElements",
+    "Numbers7x49",
+    "Numbers6x45",
+    "Numbers5x36"
 ]
 from .literals import *
 from .custom_type import *
