a210ff1e99e5412d54ee5bd52d8bf698f6f10822
Dmitry-Peskov <dv.peskov@yandex.ru>
2024-07-07 21:21:35 +0300

    refactor method extract_date_elements_from_text

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,5 +1,6 @@
 import datetime
 from custom_type import LotteryNames, LotteryNumber, DateTimeElements
+from bs4 import BeautifulSoup
 
 
 class TextConverter:
@@ -22,6 +23,11 @@ class TextConverter:
         "Sportlotto6x45": "Результаты тиража № {0} «Спортлото «6&nbsp;из&nbsp;45»,  ",
         "Sportlotto_5x36": "Результаты тиража № {0} «Спортлото «5&nbsp;из&nbsp;36»,  "
     }
+    __tags = {
+        "Sportlotto_7x49": {"class": "cleared game_567 game_7x49"},
+        "Sportlotto6x45": {"class": "cleared game_567 game_6x45"},
+        "Sportlotto_5x36": {"class": "cleared game_567 game_5x36"}
+    }
 
     @classmethod
     def __get_num_month_by_title(cls, title: str) -> int:
@@ -40,7 +46,16 @@ class TextConverter:
         raise ValueError("Для лоттереи {0} не реализована поддержка".format(lottery))
 
     @classmethod
-    def extract_date_elements_from_text(cls, lottery: LotteryNames, number: LotteryNumber, text: str) -> DateTimeElements:
+    def __get_tags(cls, lottery: LotteryNames) -> dict[str, str]:
+        tags = cls.__tags.get(lottery)
+        if tags:
+            return cls.__tags.get(lottery)
+        raise ValueError("Для лоттереи {0} не реализована поддержка".format(lottery))
+
+    @classmethod
+    def extract_date_elements_from_text(cls, lottery: LotteryNames, number: LotteryNumber, html: BeautifulSoup) -> DateTimeElements:
+        tags = cls.__get_tags(lottery)
+        text = html.find("div", attrs=tags)
         date_element = cls.__clear_datetime_string(lottery, number, text).split()
         day = int(date_element[0])
         month = cls.__get_num_month_by_title(date_element[1])
@@ -49,3 +64,8 @@ class TextConverter:
         hour, minute = int(hour_minutes[0]), int(hour_minutes[1])
         dt = datetime.datetime(year, month, day, hour, minute)
         return DateTimeElements(dt, year, month, day, hour, minute)
+
+    @classmethod
+    def extract_numbers_from_text(cls, lottery: LotteryNames):
+        pass
+
